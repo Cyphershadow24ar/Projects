3afcebd5fbea5f4b9a9716ff2d100901285dc638
Aniket Raj <aniketraj0224@gmail.com>
2024-09-18 03:06:11 +0530

    Delete Bell Reckoner directory

--- Bell Reckoner/A4/scripts.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('legalForm').addEventListener('submit', function(event){
-    event.preventDefault(); // Prevent form from submitting
-    var court = document.getElementById('court').value;
-    var lawyer = document.getElementById('lawyer').value;
-    console.log('Court selected:', court);
-    console.log('Lawyer selected:', lawyer);
-    // Implement further logic here
-});
